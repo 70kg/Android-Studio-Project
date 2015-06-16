71bf769a426703bfbf8e4787390c90781604224b
王鹏 <Mr_Wrong@Mr-WrongdeMacBook-Pro.local>
2015-06-16 20:27:57 +0800

    例行更新

---
+++ app/src/main/java/com/com/mr_wrong/CustomView/MyPullScrollView.java
@@ -0,0 +1,23 @@
+package com.com.mr_wrong.CustomView;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+/**
+ * Created by Mr_Wrong on 15/6/16.
+ */
+public class MyPullScrollView extends ScrollView {
+    private  static final
+    public MyPullScrollView(Context context) {
+        super(context);
+    }
+
+    public MyPullScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyPullScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
