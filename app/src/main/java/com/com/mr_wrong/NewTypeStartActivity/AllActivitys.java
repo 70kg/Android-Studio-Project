c46b1f0bbbdd63472903633b9f2ba15ba28a8338
王鹏 <Mr_Wrong@Mr-WrongdeMBP.lan>
2015-07-05 22:33:47 +0800

    update main activity

--- app/src/main/java/com/com/mr_wrong/NewTypeStartActivity/AllActivitys.java
+++ app/src/main/java/com/com/mr_wrong/NewTypeStartActivity/AllActivitys.java
@@ -2,15 +2,34 @@ package com.com.mr_wrong.NewTypeStartActivity;
 
 import android.app.Activity;
 
+import com.com.mr_wrong.Actionbar.ActionbarActivity;
 import com.com.mr_wrong.Bezier.BezierActivity;
 import com.com.mr_wrong.AsyncTask.AsyncTaskActivity;
+import com.com.mr_wrong.ImageLoaderWithCaches.ImageLoaderActivity;
+import com.com.mr_wrong.NumberProgressBar.NumberProgressBarActivity;
+import com.com.mr_wrong.PopButton.PopButtonActivity;
+import com.com.mr_wrong.RecyclerView.RecyclerViewActivity;
+import com.com.mr_wrong.RecyclerViewItemAnimator.ItemAnimatorActivity;
+import com.com.mr_wrong.Scroller.ScrollActivity;
+import com.com.mr_wrong.SlideView.SlideActivity;
+import com.com.mr_wrong.VolleyAndGson.VolleyActivity;
+
 /**
  * Created by Mr_Wrong on 15/7/5.
  */
 public enum AllActivitys {
     BezierActivity(BezierActivity.class),
-    AsyncTaskActivity(AsyncTaskActivity.class);
-
+    AsyncTaskActivity(AsyncTaskActivity.class),
+    ImageLoaderActivity(ImageLoaderActivity.class),
+    RecyclerViewActivity(RecyclerViewActivity.class),
+    ScrollActivity(ScrollActivity.class),
+    ItemAnimatorActivity(ItemAnimatorActivity.class),
+    VolleyActivity(VolleyActivity.class),
+    NumberProgressBarActivity(NumberProgressBarActivity.class),
+    ViewDraghelper(com.com.mr_wrong.ViewDraghelper.MainActivity.class),
+    PopButtonActivity(PopButtonActivity.class),
+    SlideActivity(SlideActivity.class),
+    ActionbarActivity(ActionbarActivity.class);
 
     private Class activityClazz;
 
