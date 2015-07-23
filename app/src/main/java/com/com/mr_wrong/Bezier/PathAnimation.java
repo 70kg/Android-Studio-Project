1277468cd1071d702129a792b3259d464a416865
70kg <985469793@qq.com>
2015-07-23 13:50:01 +0800

    pathanimation占坑

---
+++ app/src/main/java/com/com/mr_wrong/Bezier/PathAnimation.java
@@ -0,0 +1,70 @@
+package com.com.mr_wrong.Bezier;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.View;
+import android.view.WindowManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Mr_Wrong on 15/7/20.
+ */
+public class PathAnimation extends View {
+    private Path mPath;
+    private Paint mPaint;
+    private List<Fllower> fllowers = new ArrayList<>();
+    private float phase;
+
+
+    public PathAnimation(Context context) {
+        super(context);
+
+        WindowManager wm = getw
+    }
+
+
+
+    class  Fllower{
+        private float x;
+        private float y;
+        private Path path;
+
+        public float getX() {
+            return x;
+        }
+
+        public void setX(float x) {
+            this.x = x;
+        }
+
+        public float getY() {
+            return y;
+        }
+
+        public void setY(float y) {
+            this.y = y;
+        }
+
+        public Path getPath() {
+            return path;
+        }
+
+        public void setPath(Path path) {
+            this.path = path;
+        }
+
+        public float getValue() {
+            return value;
+        }
+
+        public void setValue(float value) {
+            this.value = value;
+        }
+
+        private float value;
+
+    }
+}
